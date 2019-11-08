865fab1ca2c303f6d79b3b300c8b4c21f304b0cc
DefaultUsername76 <42473991+DefaultUsername76@users.noreply.github.com>
2019-11-08 09:16:39 -0600

    Add files via upload

---
+++ CommentsDialog.java
@@ -0,0 +1,19 @@
+package Chapter1;
+/**
+ * here's a comment block
+ *
+ */
+public class Comments {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+// this is a comment line 
+		// here's another 
+		JOptionPane.showMessageDialog("Comments are not seen by users!");
+	}
+
+}
+// it can be in any place of the program
+
+
+
