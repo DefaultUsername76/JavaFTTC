92c7f795efb77086a05625bc1e58da93b3cdf88c
DefaultUsername76 <42473991+DefaultUsername76@users.noreply.github.com>
2019-11-08 09:10:21 -0600

    Add files via upload

---
+++ Comments.java
@@ -0,0 +1,18 @@
+
+/**
+ * here's a comment block
+ *
+ */
+public class Comments {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+// this is a comment line 
+		// here's another 
+	}
+
+}
+// it can be in any place of the program
+
+
+
---
+++ MovieQuote.java
@@ -0,0 +1,19 @@
+/**This program displays a movie quote
+ * 
+ */
+
+/**
+ * @author ma1015872
+ *
+ */
+public class MovieQuote {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("I'm going to make him an offer he can't refuse - Godfather(1972)");
+	}
+
+}
